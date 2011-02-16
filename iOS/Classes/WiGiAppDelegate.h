4e147881aade1587752f0180d34c5eca3d7c1715
datvikash <dat.vikash@gmail.com>
2011-02-16 14:04:18 -0500

    created iOS branch

---
+++ iOS/Classes/WiGiAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  WiGiAppDelegate.h
+//  WiGi
+//
+//  Created by Vikash Dat on 2/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WiGiAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
