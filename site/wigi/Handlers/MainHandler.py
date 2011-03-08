6d7c64f2d6272625e703333f0acbb7c40e6ee52a
vikash dat <dat.vikash@gmail.com>
2011-03-08 17:26:57 +0000

    removed yaml, placed .gitignore

--- site/wigi/Handlers/MainHandler.py
+++ site/wigi/Handlers/MainHandler.py
@@ -20,21 +20,29 @@ class MainHandler(BaseHandler,tornado.auth.FacebookGraphMixin):
             self.render("index.html", login_status='true')
 
 
+class MainHandler2(BaseHandler,tornado.auth.FacebookGraphMixin):
+    
+    def get(self):
+	self.render("test.html", login_status='true')
+
+
 class TestHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
     @tornado.web.authenticated
     @tornado.web.asynchronous
     def get(self):
-                self.facebook_request(
-                    "/me/picture",
+	logging.error("access token: %s" % self.current_user["access_token"])
+        self.facebook_request(
+                    "/me/friends",
                     access_token=self.current_user["access_token"],
                     callback=self.async_callback(self._on_post))
 
-    def _on_post(self, picture):
-        if not picture:
+    def _on_post(self, books):
+        if not books:
             # Call failed; perhaps missing permission?
             self.authorize_redirect(redirect_uri=site_config.get('wigi','facebook_redirect_uri'),
                                 client_id=self.settings["facebook_api_key"],
                                 extra_params={"scope": site_config.get('wigi','facebook_permissions_scope'),
                                               "display":"popup"})
             return
-        self.finish("Posted a message!")
+	self.write(books)
+        self.finish()
