c074236a21361bbb3b3c070661e265409487dfe8
vikash dat <dat.vikash@gmail.com>
2011-03-09 16:02:29 +0000

    creating in memory db for dev purposes in init

--- site/wigi/__init__.py
+++ site/wigi/__init__.py
@@ -1,4 +1,8 @@
 print "in __init__"
-from wigi.models.models import DBManager
+from wigi.models.models import DBManager, User
 #create a DBManger instance
 DbMan = DBManager()
+
+#for dev purposes
+#create in memory db
+User.metadata.create_all(DbMan.engine)
